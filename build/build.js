3ac6d9a13ff4f027fdd779013bf3725dc89ed1e8
pansu <77228818+sjoongh@users.noreply.github.com>
2022-01-18 16:44:45 +0900

    fix

--- build/build.js
+++ build/build.js
@@ -22,7 +22,7 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     process.stdout.write(stats.toString({
       colors: true,
       modules: false,
-      children: false, // If you are using ts-loader, setting this to true will make TypeScript errors show up during build.
+      children: false, 
       chunks: false,
       chunkModules: false
     }) + '\n\n')
@@ -33,9 +33,5 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     }
 
     console.log(chalk.cyan('  Build complete.\n'))
-    console.log(chalk.yellow(
-      '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
-    ))
   })
 })
