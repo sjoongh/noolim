963b0897659050e477138b82aa07d360b22f5fa2
pansu <77228818+sjoongh@users.noreply.github.com>
2022-01-18 17:30:40 +0900

    fix

--- src/api/account.js
+++ src/api/account.js
@@ -1,18 +1,21 @@
 import req from './req-wrapper'
 
+// URL 경로 설정
 const ACCOUNT_URI = {
   LOGIN: '/auth/login',
   USER: '/users'
 }
 
 export default {
+  // login success or fail 여부
   login (body, success, fail) {
     req.post(ACCOUNT_URI.LOGIN, body, success, fail)
   },
+  // if login succes? user data 가져옴
   getUser (success) {
     req.get(ACCOUNT_URI.USER, success)
   },
+  // 회원가입 기능 미구현
   signup () {
-    // req.
   }
 }
