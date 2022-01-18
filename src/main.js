3face10fd1011f1055390b9d0d936e6817887d47
pansu <77228818+sjoongh@users.noreply.github.com>
2022-01-18 17:16:22 +0900

    fix

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import App from "./App";
@@ -12,9 +10,14 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
+// 모든 요청에 적용될 기본 구성(Config Defaults)값을 지정
+// 글로벌 axios 기본(defaults) 설정
+// vue.js의 서버에서 모든 axios요청이 요구되므로 고정 URL로 설정
+// 즉 매번 axios 요청시마다 URL을 (헤더를)고정적으로 설정한다.
 axios.defaults.baseURL =
   "http://ec2-13-125-140-17.ap-northeast-2.compute.amazonaws.com:8080";
 
+// NAVER 지도 API 설정
 require("dotenv").config();
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
@@ -25,7 +28,6 @@ Vue.use(naver, {
   subModules: "" // 서브 모듈
 });
 
-/* eslint-disable no-new */
 new Vue({
   el: "#app",
   store,
